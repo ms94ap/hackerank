3a38eab579b9336408546bcb767de247795a8864
andrew <github email address>
2017-05-26 19:56:50 +0300

    first problem

---
+++ warmup/solve_me_first.rb
@@ -0,0 +1,11 @@
+# Code that prints the sum calculated and returned by 
+# solveMeFirst is provided for you in the editor.
+
+def solveMeFirst (a, b)
+  # Hint: Type return a+b below   
+    return a.to_i + b.to_i
+end
+val1 = gets.to_i
+val2 = gets.to_i
+sum = solveMeFirst(val1,val2)
+print (sum)
